d32f4736a9df134d05e2320b8fd89a3589fcedde
Daniel Washington Ignacio <danielvertigo@hotmail.com>
2024-01-03 13:37:57 -0300

    Initial Commit

---
+++ AvocadosUI/AvocadosUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosUIApp.swift
+//  AvocadosUI
+//
+//  Created by Daniel Washington Ignacio on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
